fcbec5c3b7d1151118a9498b837026b37c65bd2e
A Furry <afurry@users.noreply.github.com>
2018-06-19 03:51:07 +0300

    .go -- implement logging in

--- fadownloader.go
+++ fadownloader.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path"
@@ -26,7 +27,7 @@ func isResponseOK(response *http.Response) bool {
 	return false
 }
 
-func handleLinks(URL string, handler func(link *browser.Link)) error {
+func OpenURL(URL string) error {
 	ok, remaining := rl.Try()
 	if !ok {
 		fmt.Printf("Ratelimit exceeded, sleeping %v...\n", remaining)
@@ -36,18 +37,96 @@ func handleLinks(URL string, handler func(link *browser.Link)) error {
 	fmt.Printf("Opening %s...\n", URL)
 	err := bow.Open(URL)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	if !isResponseOK(bow.State().Response) {
 		fmt.Printf("Response is not ok -- stopping\n")
 		return fmt.Errorf("Response is not ok")
 	}
+	return nil
+}
+
+func handleLinks(URL string, handler func(link *browser.Link)) error {
+	err := OpenURL(URL)
+	if err != nil {
+		return err
+	}
 
 	for _, link := range bow.Links() {
 		handler(link)
 	}
 
+	return nil
+}
+
+func doLogin(loginURL string) error {
+	log.Println("Going to login page")
+	err := OpenURL(loginURL)
+	if err != nil {
+		return err
+	}
+
+	jar.Save()
+
+	foundForm := false
+	for _, form := range bow.Forms() {
+		if form == nil {
+			continue
+		}
+		if strings.Contains(form.Action(), "/login/") {
+			foundForm = true
+			log.Println("Filling form")
+			form.Input("name", "test")
+			form.Input("pass", "test")
+			log.Println("Logging in")
+			err := form.Submit()
+			if err != nil {
+				return err
+			}
+			log.Println("Got ourselves to page with title", bow.Title())
+			foundLink := false
+			for _, link := range bow.Links() {
+				if strings.Contains(link.URL.Path, "/user/") {
+					foundLink = true
+					if link.Text == "Guest" {
+						return fmt.Errorf("Could not log in!")
+					}
+				}
+			}
+			if foundLink == false {
+				return fmt.Errorf("Could not find user link after logging in!")
+			}
+			break
+		}
+	}
+	if foundForm == false {
+		return fmt.Errorf("Login page does not have login form")
+	}
+	jar.Save()
+	return nil
+}
+
+func checkAndLogin() error {
+	err := OpenURL("https://furaffinity.net/")
+	if err != nil {
+		return err
+	}
+
+	log.Println("Checking if we need to login...")
+	needLogin := false
+	var loginURL string
+	for _, link := range bow.Links() {
+		if strings.Contains(link.URL.Path, "/login/") {
+			log.Println("We need to login")
+			needLogin = true
+			loginURL = link.URL.String()
+			break
+		}
+	}
+	if needLogin {
+		return doLogin(loginURL)
+	}
 	return nil
 }
 
@@ -73,6 +152,11 @@ func main() {
 	}
 	jar.Save()
 	defer jar.Save()
+
+	err := checkAndLogin()
+	if err != nil {
+		panic(err)
+	}
 
 	startPage := "https://www.furaffinity.net/gallery/wolfy-nail/"
 
